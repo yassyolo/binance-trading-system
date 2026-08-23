c708d3ac11e2e785e0a1b485719f04b7540c02e3
Yoana Yotova <yoana.yotova03@gmail.com>
2026-08-23 13:02:25 +0300

    refactoring

--- src/TradingViewWebhookService.fix19.bak/Configuration/TradingViewBotOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TradingViewWebhookService.Configuration;
-
-public sealed class TradingViewBotOptions
-{
-    public bool Enabled { get; set; } = true;
-
-    public string Symbol { get; set; } = "BTCUSDC";
-}
-
